abf1115272dc4eccfad0cdb88b9fd22a80f1f130
Atharva Bakshi <atharva.bakshi3@gmail.com>
2025-09-01 16:59:17 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -37,6 +37,8 @@ resource "aws_subnet" "public_subnet" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
+
+  depends_on = [aws_vpc.main_vpc]
 }
 
 # Create Security Group allowing SSH and HTTP
